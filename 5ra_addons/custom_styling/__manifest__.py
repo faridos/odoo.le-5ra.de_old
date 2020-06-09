b4021c347ded3cf50655bdf8ff6f8c2177b4d892
farid <fm@vimmoda.com>
2020-06-09 17:46:39 +0200

    first commit

---
+++ 5ra_addons/custom_styling/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "Custom Styling",
+    "version": "13.0.0.0.1",
+    "sequence": 100,
+    "category": "Tools",
+    "author": "ferid.maghraoui@gmail.com",
+    "license": 'LGPL-3',
+    "description": """
+This module lets administrator to handle magazine for netzwerksüdbaden  """,
+    "depends": [
+        'web',
+        'account',
+    ],
+    "data": [
+        # Data views
+
+        # Security
+
+        # Wizard Views
+
+        # Views
+
+        # assets
+        'views/main_assets.xml'
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
